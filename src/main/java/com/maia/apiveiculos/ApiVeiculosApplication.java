5f25b74ed7822b4583f35eac8e5f71b3203568b5
dowglasmaia <dowglasmaia@gmail.com>
2021-02-04 17:33:33 -0300

    criando projeto

---
+++ src/main/java/com/maia/apiveiculos/ApiVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.maia.apiveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maia/apiveiculos/ApiVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maia.apiveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
