e04d20b9f1586c9b97a459656c99e7a59c0166ae
dowglasmaia <dowglasmaia@gmail.com>
2021-02-05 16:54:09 -0300

    criando api projeção de gastos e organizando diretório git

---
+++ api-projecao-gastos/src/main/java/com/maia/apiprojecaogastos/ApiProjecaoGastosApplication.java
@@ -0,0 +1,13 @@
+package com.maia.apiprojecaogastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjecaoGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProjecaoGastosApplication.class, args);
+	}
+
+}
